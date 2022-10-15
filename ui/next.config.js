40abca75b41ea92fc1f813d29aa5cdfa494688e1
Rahul Zhade <rzhade3@users.noreply.github.com>
2022-10-15 08:15:45 +0000

    Update next config for Pages

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  basePath: '/ascii-reactions',
+  assetPrefix: '/ascii-reactions/',
 }
 
 module.exports = nextConfig
